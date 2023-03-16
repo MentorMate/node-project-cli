3c60aaeaa5313dfda88252bb4d379ab073e27310
Emil Ivanov <emil.ivanov@mentormate.com>
2023-03-16 18:34:42 +0200

    refactor(openapi): cleanup openapi related code

--- assets/express/src/app.ts
+++ assets/express/src/app.ts
@@ -1,15 +1,10 @@
 import express from 'express';
 import pino from 'pino';
-import { ZodObject } from 'zod';
-import httpStatuses from 'statuses';
-import { OpenAPIGenerator } from '@asteasolutions/zod-to-openapi';
 
 import { initializeKnex } from './database/initilize-knex';
 import createDbRepos from '@database';
 import apiDefinitionFactory from '@api';
 import { initializeMiddlewares, validateRequest } from './api/middleware';
-import { registry } from '@modules';
-
 import { asyncHandler } from '@common';
 import { Environment } from '@common/environment';
 
@@ -25,11 +20,11 @@ export function create(env: Environment) {
     }),
   });
 
-  const app = express();
   const knex = initializeKnex(logger);
   const dbRepositories = createDbRepos(knex);
-
   const apiRoutes = apiDefinitionFactory(dbRepositories);
+
+  const app = express();
   app.use(initializeMiddlewares(logger));
 
   for (const {
@@ -49,83 +44,9 @@ export function create(env: Environment) {
     app[method](path, ...middlewares, routeHandler);
   }
 
-  // Hello world
-  app.get('/', (_req, res) => {
-    res.send('Hello world');
-  });
-
   const destroy = async () => {
     await knex.destroy();
   };
 
-  return {
-    app,
-    destroy,
-    createOpenAPIDocument: () => {
-      for (const {
-        operationId,
-        tags,
-        summary,
-        description,
-        method,
-        path,
-        request,
-        responses,
-      } of apiRoutes) {
-        registry.registerPath({
-          operationId,
-          tags,
-          summary,
-          description,
-          method,
-          path: path.replaceAll(
-            /:[a-zA-Z]+/g,
-            (match) => `{${match.substring(1)}}`
-          ),
-          ...(request && {
-            request: {
-              params: request.params,
-              query: request.query,
-              headers: request.headers,
-              ...(request.body && {
-                body: {
-                  content: {
-                    'application/json': {
-                      schema: request.body,
-                    },
-                  },
-                },
-              }),
-            },
-          }),
-          responses: Object.fromEntries(
-            Object.entries(responses).map(([code, schema]) => [
-              code,
-              schema instanceof ZodObject
-                ? {
-                    description:
-                      httpStatuses.message[code as never as number] ??
-                      'Unknown response code',
-                    content: { 'application/json': { schema } },
-                  }
-                : schema,
-            ])
-          ),
-        });
-      }
-
-      const generator = new OpenAPIGenerator(registry.definitions, '3.0.0');
-
-      const document = generator.generateDocument({
-        info: {
-          version: '1.0.0',
-          title: 'My API',
-          description: 'This is the API',
-        },
-        servers: [{ url: '' }],
-      });
-
-      return document;
-    },
-  };
+  return { app, destroy };
 }
