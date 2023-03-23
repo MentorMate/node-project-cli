3b1d4ba3b699461cb0694a031fd3b83816b6d87d
Zhitomir Oreshenski <zhitomir.oreshenski@MBGNM0002.local>
2023-03-23 15:00:27 +0200

    refactor: remove max chars validation for jwt secret

--- assets/express/src/common/environment.ts
+++ assets/express/src/common/environment.ts
@@ -3,7 +3,7 @@ import { z } from 'zod';
 const string = z.string().trim();
 const port = z.coerce.number().int().gte(1024).lte(65535);
 const expiration = z.coerce.number().int().gte(1000).or(z.string());
-const secret = z.string().trim().min(10).max(50);
+const secret = z.string().trim().min(10);
 
 export const envSchema = z.object({
   // Node
