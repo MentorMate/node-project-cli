ff071738fee7273c23680866d8d8383a120ef250
Zhitomir Oreshenski <zhitomir.oreshenski@MBGNM0002.local>
2023-03-31 12:36:15 +0300

    feat: add tests to improve the e2e coverage

--- assets/test/todos/list.spec-e2e.ts
+++ assets/test/todos/list.spec-e2e.ts
@@ -77,6 +77,19 @@ describe('GET /v1/todos', () => {
       });
     });
 
+    describe('when name = "Laundry" filter is applied', () => {
+      it('should return todo list with matched results', async () => {
+        const res = await request(app)
+          .get('/v1/todos?filters[name]=Laundry')
+          .set('Authorization', 'Bearer ' + jwtTokens.idToken)
+          .set('Accept', 'application/json');
+
+        expect(res.headers['content-type']).toMatch(/json/);
+        expect(res.status).toEqual(200);
+        expect(res.body.data.length).toEqual(3);
+      });
+    });
+
     describe('when pagination items is applied', () => {
       it('should return not completed todo list', async () => {
         const res = await request(app)
@@ -186,6 +199,22 @@ describe('GET /v1/todos', () => {
         expect(sortedRes.body.data).toStrictEqual(
           sortByField(res.body.data, 'createdAt', 'asc')
         );
+      });
+    });
+
+    describe('when all filters are applied', () => {
+      it('should return filtered todo list', async () => {
+        const res = await request(app)
+          .get(
+            '/v1/todos?filters[name]=Laundry&sorts[0][column]=name&sorts[0][order]=desc&pagination[page]=1&pagination[items]=2'
+          )
+          .set('Authorization', 'Bearer ' + jwtTokens.idToken)
+          .set('Accept', 'application/json');
+
+        expect(res.headers['content-type']).toMatch(/json/);
+        expect(res.status).toEqual(200);
+        expect(res.body.data.length).toEqual(2);
+        expect(res.body.meta.total).toEqual(3);
       });
     });
   });
