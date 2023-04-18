daefa7dd7d7aa57c44c4aecd3f69443bf08f4708
Zhitomir Oreshenski <58042315+zhitomir-oreshenski-mm@users.noreply.github.com>
2023-04-18 15:26:16 +0300

    feat: add dotenv support nest
    - added a start script and moved jest config out of generate.js
    - added jest tests

--- src/commands/generate.js
+++ src/commands/generate.js
@@ -176,62 +176,20 @@ module.exports = {
       ...packageJson.scripts,
       ...userInput.pkgJson.scripts,
     };
+
     packageJson.dependencies = {
       ...packageJson.dependencies,
       ...userInput.pkgJson.dependencies,
     };
+
     packageJson.devDependencies = {
       dotenv: '^16.0.3',
       ...packageJson.devDependencies,
       ...userInput.pkgJson.devDependencies,
     };
 
-    // TODO: move out
     if (userInput.framework === 'nest') {
-      Object.assign(packageJson.jest, {
-        coveragePathIgnorePatterns: [
-          '<rootDir>/main.ts$',
-          '<rootDir>/app.module.ts$',
-        ],
-        coverageThreshold: {
-          global: {
-            branches: 85,
-            functions: 85,
-            lines: 85,
-            statements: 85,
-          },
-        },
-      });
-
-      Object.assign(packageJson.scripts, {
-        'test:e2e':
-          'DOTENV_CONFIG_PATH=.env.test node -r dotenv/config ./node_modules/.bin/jest --config ./test/jest-e2e.json',
-        'test:e2e:cov': 'npm run test:e2e -- --coverage',
-      });
-
-      const jestE2eConfig = JSON.parse(
-        read(`${userInput.appDir}/test/jest-e2e.json`)
-      );
-
-      Object.assign(jestE2eConfig, {
-        rootDir: '..',
-        coverageDirectory: 'coverage-e2e',
-        collectCoverageFrom: ['<rootDir>/src/**/*.(t|j)s'],
-        coveragePathIgnorePatterns: [
-          '<rootDir>/src/main.ts$',
-          '<rootDir>/.*spec.ts$',
-        ],
-        coverageThreshold: {
-          global: {
-            branches: 85,
-            functions: 85,
-            lines: 85,
-            statements: 85,
-          },
-        },
-      });
-
-      write(`${userInput.appDir}/test/jest-e2e.json`, jestE2eConfig);
+      Object.assign(packageJson.jest, userInput.pkgJson.jest);
     }
 
     try {
--- src/extensions/jest-config-and-coverage-wf.js
+++ src/extensions/jest-config-and-coverage-wf.js
@@ -12,7 +12,7 @@ module.exports = (toolbox) => {
   }) => {
     const {
       print: { success, muted },
-      filesystem: { copyAsync },
+      filesystem: { copyAsync, write, read },
     } = toolbox;
 
     async function asyncOperations() {
@@ -102,6 +102,51 @@ module.exports = (toolbox) => {
           'test:e2e:db:recreate':
             'DOTENV_CONFIG_PATH=.env.test ts-node -r dotenv/config ./test/utils/recreate-db',
         });
+      }
+
+      if (framework === 'nest') {
+        pkgJson.jest = {
+          coveragePathIgnorePatterns: [
+            '<rootDir>/main.ts$',
+            '<rootDir>/app.module.ts$',
+          ],
+          coverageThreshold: {
+            global: {
+              branches: 85,
+              functions: 85,
+              lines: 85,
+              statements: 85,
+            },
+          },
+        };
+
+        Object.assign(pkgJson.scripts, {
+          'test:e2e':
+            'DOTENV_CONFIG_PATH=.env.test node -r dotenv/config ./node_modules/.bin/jest --config ./test/jest-e2e.json',
+          'test:e2e:cov': 'npm run test:e2e -- --coverage',
+        });
+
+        const jestE2eConfig = JSON.parse(read(`${appDir}/test/jest-e2e.json`));
+
+        Object.assign(jestE2eConfig, {
+          rootDir: '..',
+          coverageDirectory: 'coverage-e2e',
+          collectCoverageFrom: ['<rootDir>/src/**/*.(t|j)s'],
+          coveragePathIgnorePatterns: [
+            '<rootDir>/src/main.ts$',
+            '<rootDir>/.*spec.ts$',
+          ],
+          coverageThreshold: {
+            global: {
+              branches: 85,
+              functions: 85,
+              lines: 85,
+              statements: 85,
+            },
+          },
+        });
+
+        write(`${appDir}/test/jest-e2e.json`, jestE2eConfig);
       }
     }
 
