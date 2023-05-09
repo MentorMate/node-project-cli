cecc87849fe480eaf2664227257ac724a8647233
Emil Ivanov <emil.ivanov@mentormate.com>
2023-05-09 17:49:22 +0300

    fix: unify commitlint and commitzen config

--- assets/.commitlintrc.js
+++ assets/.commitlintrc.js
@@ -1,23 +1,6 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
-  rules: {
-    'type-enum': [
-      2,
-      'always',
-      [
-        'chore',
-        'feat',
-        'fix',
-        'docs',
-        'style',
-        'refactor',
-        'test',
-        'revert',
-        'ci',
-      ],
-    ],
-    'subject-case': [2, 'always', ['lower-case', 'sentence-case']],
-  },
+  rules: {},
   parserPreset: {
     parserOpts: {
       referenceActions: null,
